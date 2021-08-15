c3bb347b6fc7be3316d8671c75ad253b350e0bca
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-08-15 07:44:10 +0500

    Variables

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +16,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        display(1);
+        int quantity=3;
+        display(quantity);
+        displayPrice(quantity*5);
     }
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText(""+ number);
+    }
+
+    public void displayPrice(int number)
+    {
+        TextView priceTextView= (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
